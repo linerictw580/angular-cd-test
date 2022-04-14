ce0ba379edad7052ed8d61ed652df53636370cee
suvercall580 <suvercall580@gmail.com>
2022-04-14 21:57:51 +0800

    修正誤改 node-base

--- src/app/components/default-node/default-node.component.ts
+++ src/app/components/default-node/default-node.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-default-node',
   templateUrl: './default-node.component.html',
   styleUrls: ['./default-node.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class DefaultNodeComponent implements OnInit {
   @Input() title: string = 'Node';
--- src/app/components/node-base/node-base.component.ts
+++ src/app/components/node-base/node-base.component.ts
@@ -16,7 +16,7 @@ import { IButton } from 'src/app/models/IButton.model';
 import { getName } from 'src/app/utils/fake.utils';
 
 @Component({
-  selector: 'app-default-node-base',
+  selector: 'app-node-base',
   templateUrl: './node-base.component.html',
   styleUrls: ['./node-base.component.scss'],
 })
