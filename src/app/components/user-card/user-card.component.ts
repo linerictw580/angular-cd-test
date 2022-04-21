93ebfcf188b299ff0c949feb331d97362e2e2ca2
suvercall580 <suvercall580@gmail.com>
2022-04-22 00:41:37 +0800

    (1) .eslintrc.json 關閉 @angular-eslint/no-conflicting-lifecycle 規則 (避免 Lifecycle Demo 紅底線) (2) 調整 console.log 樣式 (3) 實作 ngDoCheck、ngOnDestroy (4) 新增 Trigger Timeout、Trigger Interval 按鈕

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,8 +1,11 @@
 import {
+  ChangeDetectionStrategy,
   Component,
+  DoCheck,
   EventEmitter,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   Output,
   SimpleChanges,
@@ -12,8 +15,10 @@ import {
   selector: 'app-user-card',
   templateUrl: './user-card.component.html',
   styleUrls: ['./user-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserCardComponent implements OnChanges, OnInit {
+export class UserCardComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
+  title = 'User Card';
   @Input() id: string = 'default-id';
   @Input() name: { firstName: string; lastName: string } = {
     firstName: 'firstName',
@@ -25,17 +30,44 @@ export class UserCardComponent implements OnChanges, OnInit {
   @Output() changeLastName = new EventEmitter();
   @Output() changeWholeName = new EventEmitter();
 
-  private _logStyles = 'color: #bada55; background: #222';
+  private _logStyles = 'color: #bada55; background: #222; font-size: 14px; font-weight: bold;';
+
+  private _changeDetected = false;
+  private _oldFirstName: string;
+  private _oldLastName: string;
 
   constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(`app-user-card %cngOnChanges`, this._logStyles);
-    console.log(`app-user-card`, JSON.parse(JSON.stringify(changes)));
+    this._changeDetected = true;
+    console.log(`[Lifecycle Demo] app-user-card %cngOnChanges`, this._logStyles);
+    for (const prop in changes) {
+      const change = changes[prop];
+      const cur = JSON.stringify(change.currentValue);
+      const prev = JSON.stringify(change.previousValue);
+      console.log(
+        `[Lifecycle Demo] app-user-card changes\n[${prop}]: currentValue = %c${cur}%c, previousValue = %c${prev}`,
+        'color: lightgreen; background: #222;',
+        'color: black;',
+        'color: pink; background: #222;'
+      );
+      // console.log(`[Lifecycle Demo] app-user-card changes\n${prop}: currentValue = ${cur}, previousValue = ${prev}`);
+    }
   }
 
   ngOnInit(): void {
-    console.log(`app-user-card %cngOnInit`, this._logStyles);
+    console.log(`[Lifecycle Demo] app-user-card %cngOnInit`, this._logStyles);
+    // 取消註解 debugger 以查看 ngOnInit 觸發時，有部分的 Component 尚未被 render 出來
+    // (HTML Template 有使用到 string interpolation 的部分不會被 render 出來)
+    // debugger;
+  }
+
+  ngDoCheck(): void {
+    this.customChangeCheck(false);
+  }
+
+  ngOnDestroy(): void {
+    console.log(`[Lifecycle Demo] app-user-card %cngOnDestroy`, this._logStyles);
   }
 
   onChangeId() {
@@ -52,5 +84,35 @@ export class UserCardComponent implements OnChanges, OnInit {
 
   onChangeWholeName() {
     this.changeWholeName.emit();
+  }
+
+  customChangeCheck(showChangeDetail: boolean) {
+    let changeLog = '';
+    if (this.name.firstName !== this._oldFirstName) {
+      this._changeDetected = true;
+      changeLog = `[Lifecycle Demo] app-user-card Custom Do Check\n[firstName]: currentValue = %c${this.name.firstName}%c, previousValue = %c${this._oldFirstName}`;
+      this._oldFirstName = this.name.firstName;
+    }
+
+    if (this.name.lastName !== this._oldLastName) {
+      this._changeDetected = true;
+      changeLog = `[Lifecycle Demo] app-user-card Custom Do Check\n[lastName]: currentValue = %c${this.name.lastName}%c, previousValue = %c${this._oldLastName}`;
+      this._oldLastName = this.name.lastName;
+    }
+
+    if (this._changeDetected) {
+      console.log(`[Lifecycle Demo] app-user-card %cngDoCheck`, this._logStyles);
+      if (showChangeDetail) {
+        console.log(
+          changeLog,
+          'color: lightgreen; background: #222;',
+          'color: black;',
+          'color: pink; background: #222;'
+        );
+      }
+    } else {
+      console.log(`[Lifecycle Demo] app-user-card %cngDoCheck: no changes`, this._logStyles);
+    }
+    this._changeDetected = false;
   }
 }
--- src/app/demos/lifecycle-demo/lifecycle-demo.component.ts
+++ src/app/demos/lifecycle-demo/lifecycle-demo.component.ts
@@ -7,12 +7,14 @@ import faker from '@faker-js/faker';
   styleUrls: ['./lifecycle-demo.component.scss'],
 })
 export class LifecycleDemoComponent implements OnInit {
+  displayCard: boolean;
   id: string;
   name: { firstName: string; lastName: string };
 
   constructor() {}
 
   ngOnInit(): void {
+    this.displayCard = false;
     this.id = this.getId();
     this.name = this.getName();
   }
@@ -36,6 +38,31 @@ export class LifecycleDemoComponent implements OnInit {
     return faker.name.lastName();
   }
 
+  onCreateCard() {
+    this.displayCard = true;
+  }
+
+  onTriggerTimeout() {
+    console.log('[Lifecycle Demo] timeout start');
+    const timeout = setTimeout(() => {
+      clearTimeout(timeout);
+      console.log('[Lifecycle Demo] timeout end');
+    }, 1000);
+  }
+
+  onTriggerInterval(max: number) {
+    console.log('[Lifecycle Demo] interval start');
+    let count = 0;
+    const interval = setInterval(() => {
+      count++;
+      console.log('[Lifecycle Demo] interval', count);
+      if (count === max) {
+        clearInterval(interval);
+        console.log('[Lifecycle Demo] interval end');
+      }
+    }, 1000);
+  }
+
   onChangeId() {
     this.id = this.getId();
   }
@@ -49,7 +76,11 @@ export class LifecycleDemoComponent implements OnInit {
   }
 
   onChangeWholeName() {
-    // this.name = this.getName();
-    this.name = { ...this.name };
+    this.name = this.getName();
+    // this.name = { ...this.name };
+  }
+
+  onDestroyCard() {
+    this.displayCard = false;
   }
 }
